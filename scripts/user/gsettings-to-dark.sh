fd2084db93fa30353be4369f696c1573abdbd820
Federico Mahfoud <fedemahf@users.noreply.github.com>
2022-07-03 22:52:55 -0400

    update gsettings scripts

--- scripts/user/gsettings-to-dark.sh
+++ scripts/user/gsettings-to-dark.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+if ! [ -x "$(command -v gsettings)" ]; then
+	echo "libglib2.0-dev is not installed" >&2
+	exit 1
+fi
+
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+exit 0
