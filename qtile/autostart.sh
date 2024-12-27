4a0307511ca5814c3f1eb52b1c73b0d558bf14b3
Federico Mahfoud <fedemahf@users.noreply.github.com>
2024-12-27 08:36:07 -0300

    qtile: add xautolock & slock in autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -6,3 +6,9 @@ echo "Executing ~/.config/qtile/autostart.sh ..."
 if [ -x "$(command -v nm-applet)" ]; then
 	nm-applet &
 fi
+
+# Check for xautolock & slock
+if [ -x /usr/bin/xautolock ] && [ -x /usr/bin/slock ]; then
+	# Enable slock after 15 minutes idle
+	/usr/bin/xautolock -time 15 -locker slock &
+fi
